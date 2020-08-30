d2731e280052b68bb629323c19c38367d5c8ade3
Vanishta Changea <vanishta.changea@gmail.com>
2020-08-30 15:58:50 +0200

    First Commit Before Interface

---
+++ src/CustomMap.ts
@@ -0,0 +1,51 @@
+import {User} from './User';
+import {Company} from './Company';
+
+//Instruction to every other class on how they can be an argument to AddMarker
+interface Mappable{
+    location:{
+        lat:number;
+        lng:number;
+    }
+}
+
+export class CustomMap {
+    private googleMap: google.maps.Map;
+    constructor(divId:string) {
+        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        });
+    }
+    addMarker(mappable:Mappable):void{
+        new google.maps.Marker({
+            map:this.googleMap,
+            position:{
+                lat:mappable.location.lat,
+                lng:mappable.location.lng
+            }
+        })
+    }
+/*     addMarker(mappable:User | Company):void{
+        new google.maps.Marker({
+            map:this.googleMap,
+            position:{
+                lat:mappable.location.lat,
+                lng:mappable.location.lng
+            }
+        })
+    } */
+/*     addCompanyMarker(company:Company):void{
+        new google.maps.Marker({
+            map:this.googleMap,
+            position:{
+                lat:company.location.lat,
+                lng:company.location.lng
+            }
+        })
+    } */
+
+}
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+ import {User} from './User';
+import {Company} from './Company'; 
+import {CustomMap} from './CustomMap';
+
+const user = new User();
+const customMap = new CustomMap('map');
+customMap.addMarker(user);
+
+const company = new Company();
+customMap.addMarker(company);
